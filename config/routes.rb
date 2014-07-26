c073c4dae92610d8c61d17437881bcff13cd3d3f
Beibit Zhumabekov <zhumabekov@gmail.com>
2014-07-26 09:13:34 +0000

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  resources :posts
+  #get 'static_pages/home'
+
+  #get 'static_pages/help'
+
+  #get 'static_pages/about'
+  
+  #get 'static_pages/contact'
+  
+  get '/help', to: 'static_pages#help'
+  
+  get '/about', to: 'static_pages#about'
+  
+  get '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
